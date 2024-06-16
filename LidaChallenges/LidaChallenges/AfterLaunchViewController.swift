f26d19c666e556857900f12c423aa4fceea9a04e
ihar.karunny <ihar.karunny@viber.com>
2024-06-16 20:10:02 +0300

    Initial commit of application

---
+++ LidaChallenges/LidaChallenges/AfterLaunchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AfterLaunchViewController.swift
+//  LidaChallenges
+//
+//  Created by Ihar Karunny on 6/16/24.
+//
+
+import UIKit
+
+class AfterLaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
