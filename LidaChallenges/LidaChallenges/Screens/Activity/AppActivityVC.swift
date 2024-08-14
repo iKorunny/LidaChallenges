996b651a67aa1004822827a38065bf087e2dba81
ihar.karunny <ihar.karunny@viber.com>
2024-08-14 12:02:55 +0300

    Activity changed to syste,

--- LidaChallenges/LidaChallenges/Screens/Activity/AppActivityVC.swift
+++ LidaChallenges/LidaChallenges/Screens/Activity/AppActivityVC.swift
@@ -14,38 +14,26 @@ protocol AppActivityView {
 
 final class AppActivityVC: UIViewController, AppActivityView {
     
-    private lazy var loadingImage: UIImageView = {
-        let imageView = UIImageView(image: .init(named: "AppLoader"))
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
-    
-    private lazy var animation: CABasicAnimation = {
-        let rotation: CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
-        
-        rotation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
-        rotation.toValue = -CGFloat.pi * 4
-        rotation.duration = 2
-        rotation.isCumulative = true
-        rotation.autoreverses = true
-        rotation.repeatCount = .greatestFiniteMagnitude
-        return rotation
+    private lazy var activityView: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView(style: .large)
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        return indicator
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .black.withAlphaComponent(0.1)
-        view.addSubview(loadingImage)
-        loadingImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        loadingImage.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        view.addSubview(activityView)
+        activityView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        activityView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
     
     func startAnimation() {
-        loadingImage.layer.add(animation, forKey: "rotationAnimation")
+        activityView.startAnimating()
     }
     
     func stopAnimation() {
-        loadingImage.layer.removeAnimation(forKey: "rotationAnimation")
+        activityView.stopAnimating()
     }
 }
